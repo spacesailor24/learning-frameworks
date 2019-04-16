3b4135f1b1f98fb38620b59d719e82668123d2c5
Wyatt Barnes <wyattbarnes@protonmail.com>
2019-04-15 20:54:48 -0500

    Add javascript > vuejs > todo-app

---
+++ javascript/vuejs/todo-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    Hello World!
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {}
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
